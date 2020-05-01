2f94f4db961eac6537e53d297f99d8dc859582b4
yinqiwen <yinqiwen@gmail.com>
2020-05-01 21:42:09 +0800

    fix

--- src/utils/buf.rs
+++ src/utils/buf.rs
@@ -8,8 +8,8 @@ pub fn fill_read_buf(src: &mut BytesMut, dst: &mut [u8]) -> usize {
         return 0;
     }
     let mut n = src.len();
-    if n > src.len() {
-        n = src.len();
+    if n > dst.len() {
+        n = dst.len();
     }
     dst[0..n].copy_from_slice(&src[0..n]);
     src.advance(n);
